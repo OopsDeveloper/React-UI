23b43751144e250ff2aac2757dd28e0a02b5fdb8
고용수 <ajemxkem@gmail.com>
2025-04-12 02:58:48 +0900

    {} 추가

--- src/app/[...item]/page.tsx
+++ src/app/[...item]/page.tsx
@@ -5,7 +5,7 @@ import { ROUTE_PATH, isParentRoute, routePaths, routes } from '@/routes'
 const ItemPage = ({ params: { item } }: { params: { item: string[] } }) => {
   const path = ['', ...item].join('/') as ROUTE_PATH
   const route = routes[path]
-  if (!routePaths.includes(path) || isParentRoute(route) || !route.children) return null
+  if (!routePaths.includes(path) || isParentRoute(route) || !route.children) { return null; }
 
   const Component = route.children
   return <Component />
